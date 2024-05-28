8b0533a75c7f5fbd41852c119c79a99c489d7a4d
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-05-28 10:18:59 -0300

    :sparkles: Add new currency

--- argendolar.py
+++ argendolar.py
@@ -1,8 +1,10 @@
+from typing import List
+
 import requests
 
-from tipo_dolares import TipoDivisas
+import pandas as pd
 
-from typing import List
+from tipo_dolares import TipoDivisas
 
 
 class Argendolar:
@@ -10,91 +12,100 @@ class Argendolar:
         self.base_url = "https://dolarapi.com"
         self.historico_url = "https://api.argentinadatos.com"
 
-    def get_dolar(self) -> dict:
+    def get_dolar(self) -> pd.DataFrame:
         url = f"{self.base_url}/v1/dolares"
         response = requests.get(url)
         data = response.json()
-        return data
+        df = pd.DataFrame(data)
+        return df
 
-    def get_oficial(self) -> dict:
+    def get_oficial(self) -> pd.DataFrame:
         url = f"{self.base_url}/v1/dolares/oficial"
         response = requests.get(url)
         data = response.json()
-        return data
+        df = pd.DataFrame([data])
+        return df
 
-    def get_blue(self) -> dict:
+    def get_blue(self) -> pd.DataFrame:
         url = f"{self.base_url}/v1/dolares/blue"
         response = requests.get(url)
         data = response.json()
-        return data
+        df = pd.DataFrame([data])
+        return df
 
-    def get_mep(self) -> dict:
+    def get_mep(self) -> pd.DataFrame:
         url = f"{self.base_url}/v1/dolares/bolsa"
         response = requests.get(url)
         data = response.json()
-        return data
+        df = pd.DataFrame([data])
+        return df
 
-    def get_ccl(self) -> dict:
+    def get_ccl(self) -> pd.DataFrame:
         url = f"{self.base_url}/v1/dolares/contadoconliqui"
         response = requests.get(url)
         data = response.json()
-        return data
+        df = pd.DataFrame([data])
+        return df
 
-    def get_tarjeta(self) -> dict:
+    def get_tarjeta(self) -> pd.DataFrame:
         url = f"{self.base_url}/v1/dolares/tarjeta"
         response = requests.get(url)
         data = response.json()
-        return data
+        df = pd.DataFrame([data])
+        return df
 
-    def get_mayorista(self) -> dict:
+    def get_mayorista(self) -> pd.DataFrame:
         url = f"{self.base_url}/v1/dolares/mayorista"
         response = requests.get(url)
         data = response.json()
-        return data
+        df = pd.DataFrame([data])
+        return df
 
-    def get_cripto(self) -> dict:
+    def get_cripto(self) -> pd.DataFrame:
         url = f"{self.base_url}/v1/dolares/cripto"
         response = requests.get(url)
         data = response.json()
-        return data
+        df = pd.DataFrame([data])
+        return df
 
-    def get_euro(self) -> dict:
+    def get_euro(self) -> pd.DataFrame:
         url = f"{self.base_url}/v1/cotizaciones/eur"
         response = requests.get(url)
         data = response.json()
-        return data
+        df = pd.DataFrame([data])
+        return df
 
-    def get_real(self) -> dict:
+    def get_real(self) -> pd.DataFrame:
         url = f"{self.base_url}/v1/cotizaciones/brl"
         response = requests.get(url)
         data = response.json()
-        return data
+        df = pd.DataFrame([data])
+        return df
 
-    def get_chileno(self) -> dict:
+    def get_chileno(self) -> pd.DataFrame:
         url = f"{self.base_url}/v1/cotizaciones/clp"
         response = requests.get(url)
         data = response.json()
-        return data
+        df = pd.DataFrame([data])
+        return df
 
-    def get_uruguayo(self) -> dict:
+    def get_uruguayo(self) -> pd.DataFrame:
         url = f"{self.base_url}/v1/cotizaciones/uyu"
         response = requests.get(url)
         data = response.json()
-        return data
+        df = pd.DataFrame([data])
+        return df
 
-    def get_dolar_historico(self, tipo: TipoDivisas, fecha: str) -> List[dict]:
-        print("Tipo de divisas:", tipo)
-        print("Fecha:", fecha)
-        print(tipo.value)
+    def get_dolar_historico(self, tipo: TipoDivisas, fecha: str) -> pd.DataFrame:
         url = f"{self.historico_url}/v1/cotizaciones/dolares/{tipo.value}/{fecha}"
-        print(url)
         response = requests.get(url)
-        type(response)
         data = response.json()
-        return data
+        df = pd.DataFrame([data])
+        return df
 
-    def get_dolar_historia_completa(self, tipo: TipoDivisas) -> List[dict]:
+    def get_dolar_historia_completa(self, tipo: TipoDivisas) -> pd.DataFrame:
         url = f"{self.historico_url}/v1/cotizaciones/dolares/{tipo.value}"
         response = requests.get(url)
         data = response.json()
-        return data
+        df = pd.DataFrame(data)
+        return df
