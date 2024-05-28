a2ff922d727fa8bb410b85f4b6d33295122bcdf4
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-05-28 13:04:59 -0300

    :bookmark: V1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='argendolar',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(),
     install_requires=['pandas', 'requests'],
     author='Mariano Gobea Alcoba',
