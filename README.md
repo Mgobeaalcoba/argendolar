1991959b143838d554d205e3fdea6cfbf530360f
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-05-28 10:46:06 -0300

    :memo: Add README.md with examples and details

---
+++ README.md
@@ -0,0 +1,114 @@
+# Argendolar Package
+
+## Description
+
+This package is used to get the exchange rate between the Argentine Peso and the US Dollar and other currencies (Euro, Real, Peso Chileno y Peso Uruguayo). For this, it uses the [Dolar API](https://dolarapi.com) and the [ArgentinaDatos API](https://argentinadatos.com/docs/) and then it converts the obtained data to a Pandas DataFrame for further analysis.
+
+## Installation
+
+To install the package, you can use the following command:
+
+```bash
+pip install argendolar
+```
+
+## Usage
+
+To use the package, you can import the `Argendolar` class and create an instance of it. 
+ 
+```python
+from argendolar import Argendolar
+
+# Create an instance of Argendolar
+argendolar = Argendolar()
+```
+
+## Methods
+
+The `Argendolar` class has the following methods:
+
+- `get_dolar()`: Get all the dollars exchange rates: official, blue, mep, ccl, tarjeta, mayorista, etc. 
+- `get_oficial()`: Get the official dollar exchange rate.
+- `get_blue()`: Get the blue dollar exchange rate.
+- `get_mep()`: Get the MEP dollar exchange rate.
+- `get_ccl()`: Get the CCL dollar exchange rate.
+- `get_tarjeta()`: Get the dollar exchange rate for credit cards.
+- `get_mayorista()`: Get the dollar exchange rate for wholesale.
+- `get_cripo()`: Get the dollar exchange rate for cripto.
+- `get_euro()`: Get the euro exchange rate.
+- `get_real()`: Get the real exchange rate.
+- `get_chileno()`: Get the peso chileno exchange rate.
+- `get_uruguayo()`: Get the peso uruguayo exchange rate.
+- `get_dolar_historico()`: Get the historical dollar exchange rate for a specific date and Dollar Type (TipoDivisa.OFICIAL, TipoDivisa.BLUE, TipoDivisa.MEP, TipoDivisa.CCL, TipoDivisa.TARJETA, TipoDivisa.MAYORISTA, TipoDivisa.CRIPTO)
+- `get_dolar_historia_completa()`: Get the complete historical dollar exchange rate since 2011.
+
+## Examples
+
+Here are some examples of how to use the package:
+
+```python
+from argendolar import Argendolar
+
+# Create an instance of Argendolar
+argendolar = Argendolar()
+
+# Get the official dollar exchange rate
+official = argendolar.get_oficial()
+
+# Get the blue dollar exchange rate
+blue = argendolar.get_blue()
+
+# Get the MEP dollar exchange rate
+mep = argendolar.get_mep()
+
+# Get the CCL dollar exchange rate
+ccl = argendolar.get_ccl()
+
+# Get the dollar exchange rate for credit cards
+tarjeta = argendolar.get_tarjeta()
+
+# Get the dollar exchange rate for wholesale
+mayorista = argendolar.get_mayorista()
+
+# Get the dollar exchange rate for cripto
+cripto = argendolar.get_cripo()
+
+# Get the euro exchange rate
+euro = argendolar.get_euro()
+
+# Get the real exchange rate
+real = argendolar.get_real()
+
+# Get the peso chileno exchange rate
+chileno = argendolar.get_chileno()
+
+# Get the peso uruguayo exchange rate
+uruguayo = argendolar.get_uruguayo()
+
+# Get the historical dollar exchange rate for a specific date
+historico = argendolar.get_dolar_historico(tipo: TipoDivisa.MEP, fecha='2022-01-01')
+
+# Get the complete historical dollar exchange rate since 2011
+historia_completa = argendolar.get_dolar_historia_completa()
+```
+
+## Contributing
+
+If you want to contribute to this project, you can follow the steps below:
+
+1. Fork the repository to your own GitHub account.
+2. Clone the repository to your local machine.
+3. Create a new branch for your changes.
+4. Make your changes and commit them.
+5. Push your changes to your forked repository.
+6. Create a pull request from your forked repository to the original repository.
+7. Wait for the review and approval of your pull request.
+8. Once your pull request is approved, your changes will be merged into the main branch of the original repository.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
+
+## Contact
+
+If you have any questions or suggestions, you can contact me at [Mariano Gobea Alcoba](mailto:gobeamariano@gmail.com) or [Mariano Gobea Alcoba](https://www.linkedin.com/in/mariano-gobea-alcoba/).
--- argendolar.py
+++ argendolar.py
@@ -1,5 +1,3 @@
-from typing import List
-
 import requests
 
 import pandas as pd
@@ -9,10 +7,29 @@ from tipo_dolares import TipoDivisas
 
 class Argendolar:
     def __init__(self):
+        """
+        Initialize the Argendolar class.
+        """
         self.base_url = "https://dolarapi.com"
         self.historico_url = "https://api.argentinadatos.com"
 
     def get_dolar(self) -> pd.DataFrame:
+        """
+        Get all the dollars exchange rates:
+            - Oficial
+            - Blue
+            - MEP
+            - CCL
+            - Tarjeta
+            - Mayorista
+            - Cripto
+        And the exchange rates for the following currencies:
+            - Euro
+            - Real
+            - Chileno
+            - Uruguayo
+        :return: A Pandas DataFrame with the exchange rates
+        """
         url = f"{self.base_url}/v1/dolares"
         response = requests.get(url)
         data = response.json()
@@ -20,6 +37,10 @@ class Argendolar:
         return df
 
     def get_oficial(self) -> pd.DataFrame:
+        """
+        Get the exchange rate for the official dollar.
+        :return: A Pandas DataFrame with the exchange rate for the official dollar.
+        """
         url = f"{self.base_url}/v1/dolares/oficial"
         response = requests.get(url)
         data = response.json()
@@ -27,6 +48,10 @@ class Argendolar:
         return df
 
     def get_blue(self) -> pd.DataFrame:
+        """
+        Get the exchange rate for the blue dollar.
+        :return: A Pandas DataFrame with the exchange rate for the blue dollar.
+        """
         url = f"{self.base_url}/v1/dolares/blue"
         response = requests.get(url)
         data = response.json()
@@ -34,6 +59,10 @@ class Argendolar:
         return df
 
     def get_mep(self) -> pd.DataFrame:
+        """
+        Get the exchange rate for the MEP dollar.
+        :return: A Pandas DataFrame with the exchange rate for the MEP dollar.
+        """
         url = f"{self.base_url}/v1/dolares/bolsa"
         response = requests.get(url)
         data = response.json()
@@ -41,6 +70,10 @@ class Argendolar:
         return df
 
     def get_ccl(self) -> pd.DataFrame:
+        """
+        Get the exchange rate for the CCL dollar.
+        :return: A Pandas DataFrame with the exchange rate for the CCL dollar.
+        """
         url = f"{self.base_url}/v1/dolares/contadoconliqui"
         response = requests.get(url)
         data = response.json()
@@ -48,6 +81,10 @@ class Argendolar:
         return df
 
     def get_tarjeta(self) -> pd.DataFrame:
+        """
+        Get the exchange rate for the tarjeta dollar.
+        :return: A Pandas DataFrame with the exchange rate for the tarjeta dollar.
+        """
         url = f"{self.base_url}/v1/dolares/tarjeta"
         response = requests.get(url)
         data = response.json()
@@ -55,6 +92,10 @@ class Argendolar:
         return df
 
     def get_mayorista(self) -> pd.DataFrame:
+        """
+        Get the exchange rate for the mayorista dollar.
+        :return: A Pandas DataFrame with the exchange rate for the mayorista dollar.
+        """
         url = f"{self.base_url}/v1/dolares/mayorista"
         response = requests.get(url)
         data = response.json()
@@ -62,6 +103,10 @@ class Argendolar:
         return df
 
     def get_cripto(self) -> pd.DataFrame:
+        """
+        Get the exchange rate for the cripto dollar.
+        :return: A Pandas DataFrame with the exchange rate for the cripto dollar.
+        """
         url = f"{self.base_url}/v1/dolares/cripto"
         response = requests.get(url)
         data = response.json()
@@ -69,6 +114,10 @@ class Argendolar:
         return df
 
     def get_euro(self) -> pd.DataFrame:
+        """
+        Get the exchange rate for the euro.
+        :return: A Pandas DataFrame with the exchange rate for the euro.
+        """
         url = f"{self.base_url}/v1/cotizaciones/eur"
         response = requests.get(url)
         data = response.json()
@@ -76,6 +125,10 @@ class Argendolar:
         return df
 
     def get_real(self) -> pd.DataFrame:
+        """
+        Get the exchange rate for the real.
+        :return: A Pandas DataFrame with the exchange rate for the real.
+        """
         url = f"{self.base_url}/v1/cotizaciones/brl"
         response = requests.get(url)
         data = response.json()
@@ -83,6 +136,10 @@ class Argendolar:
         return df
 
     def get_chileno(self) -> pd.DataFrame:
+        """
+        Get the exchange rate for the chileno.
+        :return: A Pandas DataFrame with the exchange rate for the 'peso chileno'.
+        """
         url = f"{self.base_url}/v1/cotizaciones/clp"
         response = requests.get(url)
         data = response.json()
@@ -90,6 +147,10 @@ class Argendolar:
         return df
 
     def get_uruguayo(self) -> pd.DataFrame:
+        """
+        Get the exchange rate for the 'peso uruguayo'.
+        :return: A Pandas DataFrame with the exchange rate for the 'peso uruguayo'.
+        """
         url = f"{self.base_url}/v1/cotizaciones/uyu"
         response = requests.get(url)
         data = response.json()
@@ -97,6 +158,12 @@ class Argendolar:
         return df
 
     def get_dolar_historico(self, tipo: TipoDivisas, fecha: str) -> pd.DataFrame:
+        """
+        Get the historical exchange rate for a specific date.
+        :param tipo: The type of dollar to get the historical exchange rate for. (TipoDivisas: OFICIAL, BLUE, MEP, MAYORISTA, SOLIDARIO, TURISTA)
+        :param fecha: The date to get the historical exchange rate for. (str: "YYYY/MM/DD")
+        :return: A Pandas DataFrame with the historical exchange rate for the specified date.
+        """
         url = f"{self.historico_url}/v1/cotizaciones/dolares/{tipo.value}/{fecha}"
         response = requests.get(url)
         data = response.json()
@@ -104,6 +171,11 @@ class Argendolar:
         return df
 
     def get_dolar_historia_completa(self, tipo: TipoDivisas) -> pd.DataFrame:
+        """
+        Get the complete historical exchange rate since 2011 for a specific type of dollar.
+        :param tipo: The type of dollar to get the complete historical exchange rate for. (TipoDivisas: OFICIAL, BLUE, MEP, MAYORISTA, SOLIDARIO, TURISTA)
+        :return: A Pandas DataFrame with the complete historical exchange rate for the specified type of dollar.
+        """
         url = f"{self.historico_url}/v1/cotizaciones/dolares/{tipo.value}"
         response = requests.get(url)
         data = response.json()
